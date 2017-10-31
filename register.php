2d429890a50dd707e8c3116f29f1aa35d8e89208
Mayuresh Mlind Vartak <mayureshvartak0000@gmail.com>
2017-10-31 14:21:45 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,71 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UPSC FEVER </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<link rel="shortcut icon" icon href="p1.png"  />
+<body>
+<center><img src="p1.png" alt="HTML5 Icon" style="width:1200px;height:250px;"></center>
+	<div class="header">
+		<h2>Register</h2>
+	</div>
+	<form method="post" action="register.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" value="<?php echo $username; ?>">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="email" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			<label>Date of Birth</label>
+			<input type="Date" name="bday" placeholder="Date OF Birth">
+		</div>
+		
+		<div class="input-group">
+             <label>State</label>
+			 <input list="State" name="browser">
+  <datalist id="State">
+    <option value="Maharashtra">
+    <option value="Gujarat">
+    <option value="Madhya Pradesh">
+    <option value="Kerala">
+    <option value="Tamil-Nadu">
+  </datalist>
+		</div>
+		
+		<div class="input-group">
+             <label>City/Town</label>
+			 <input list="City" name="browser">
+  <datalist id="City">
+    <option value="Mumbai">
+    <option value="Ahmedabad">
+    <option value="Bhopal">
+    <option value="Thiruvananthapuram">
+    <option value="Chennai">
+  </datalist>
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="reg_user">Register</button>
+		</div>
+		<p>
+			Already a member? <a href="login.php">Sign in</a>
+		</p>
+	</form>
+</body>
+</html>
